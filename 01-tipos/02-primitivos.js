3fe4185cd353d4c12af5c500262240d94b639346
ZebC04 <Sebascp0412@gmail.com>
2025-05-05 20:29:23 -0600

    Se agrega variables, primitivos, constantes, tipado dinamico y comentarios

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,8 @@
+// Datos Primitivos
+let numero = 1; // Nummber
+let texto = "Hola Mundo"; //String 
+let verdadero = true; //Bool 
+let falso = false;  //Bool
+let noDefinido; // Undefined 
+let undef = undefined;  // Undefined Asignado 
+let nulo = null; //Null 
---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,16 @@
+// Comentario 
+/*
+Manera oficial de js 
+*/ 
+
+
+
+/**
+ * Comentario 
+ * de 
+ * varias 
+ * lineas 
+ */
+let numero = 42; // Otro comentario 
+
+console.log(numero); 
